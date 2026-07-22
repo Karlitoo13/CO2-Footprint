793b830357763aaefa2b89172626241822ed69bb
Karlitoo13 <karlalbers02@gmail.com>
2026-07-22 18:43:15 +0200

    data.js erstellt

---
+++ js/data.js
@@ -0,0 +1,12 @@
+const EMISSIONS_DATEN = [
+  { "land": "Deutschland", "unternehmen": "StahlWerk AG",     "branche": "Schwerindustrie", "emissionen": 48.6, "jahr": 2024 },
+  { "land": "Deutschland", "unternehmen": "EnergiePlus SE",   "branche": "Energie",         "emissionen": 71.9, "jahr": 2024 },
+  { "land": "USA",         "unternehmen": "GlobalOil Corp",   "branche": "Energie",         "emissionen": 112.4, "jahr": 2024 },
+  { "land": "USA",         "unternehmen": "TechCloud Inc",    "branche": "IT",              "emissionen": 18.7, "jahr": 2024 },
+  { "land": "China",       "unternehmen": "Dragon Steel Co",  "branche": "Schwerindustrie", "emissionen": 143.2, "jahr": 2024 },
+  { "land": "China",       "unternehmen": "SolarPanel Group", "branche": "Energie",         "emissionen": 39.5, "jahr": 2024 },
+  { "land": "Frankreich",  "unternehmen": "AeroFrance SA",    "branche": "Luftfahrt",       "emissionen": 55.0, "jahr": 2024 },
+  { "land": "Indien",      "unternehmen": "Bharat Cement",    "branche": "Baustoffe",       "emissionen": 88.6, "jahr": 2024 },
+  { "land": "Brasilien",   "unternehmen": "AmazonAgro SA",    "branche": "Landwirtschaft",  "emissionen": 63.7, "jahr": 2024 },
+  { "land": "Japan",       "unternehmen": "Nippon Motors",    "branche": "Automobil",       "emissionen": 29.8, "jahr": 2024 }
+];
